87e2443e38a107b6184076213452237d1896ac7d
Jonás Alvarado <jxalvarado88@hotmail.com>
2015-11-09 17:28:52 -0500

    Initial Commit

---
+++ Save 4 ALL - From iOS.jsx
@@ -0,0 +1,91 @@
+﻿/**
+* Remixer: @herkulano (http://www.herkulano.com)
+* Thanks to: Niels Bosma (niels.bosma@motorola.com)
+*/
+
+var folder = Folder.selectDialog();
+var document = app.activeDocument;
+
+if (document && folder) {
+    
+    var folderName = document.name.slice (0, -3);
+    var fatherFolder = new Folder(folder.fsName + "/" + folderName);
+    
+    if (!fatherFolder.exists) {
+		fatherFolder.create();
+	}
+    saveToResIOs(50, "iOS", "");
+    saveToResIOs(100, "iOS", "@2x");
+    saveToResIOs(150, "iOS", "@3x");	
+    saveToRes(42.26, "ldpi");
+    saveToRes(56.34, "mdpi");
+    saveToRes(84.51, "hdpi");
+    saveToRes(112.68, "xhdpi");
+    saveToRes(169.02, "xxhdpi");
+    saveToRes(225.36, "xxxhdpi");
+}
+
+function saveToRes(scaleTo, resFolderName) {
+    
+	var i, ab,
+		file, options,
+		resFolder;
+        
+      
+        
+
+	resFolder = new Folder(fatherFolder.fsName + "/drawable-" + resFolderName);
+
+	if (!resFolder.exists) {
+		resFolder.create();
+	}
+
+	for (i = document.artboards.length - 1; i >= 0; i--) {
+		document.artboards.setActiveArtboardIndex(i);
+		ab = document.artboards[i];
+
+		if (ab.name.indexOf("!") === -1) {
+			file = new File(resFolder.fsName + "/" + ab.name + ".png");
+
+			options = new ExportOptionsPNG24();
+			options.antiAliasing = true;
+			options.transparency = true;
+			options.artBoardClipping = true;
+			options.verticalScale = scaleTo;
+			options.horizontalScale = scaleTo;
+
+			document.exportFile(file, ExportType.PNG24, options);
+		};
+	}
+}
+
+function saveToResIOs(scaleTo, resFolderName, suffix) {
+    
+	var i, ab,
+		file, options,
+		resFolder;
+        
+	resFolder = new Folder(fatherFolder.fsName + "/" + resFolderName);
+
+	if (!resFolder.exists) {
+		resFolder.create();
+	}
+
+	for (i = document.artboards.length - 1; i >= 0; i--) {
+		document.artboards.setActiveArtboardIndex(i);
+		ab = document.artboards[i];
+
+		if (ab.name.indexOf("!") === -1) {
+			file = new File(resFolder.fsName + "/" + ab.name + suffix + ".png");
+
+			options = new ExportOptionsPNG24();
+			options.antiAliasing = true;
+			options.transparency = true;
+			options.artBoardClipping = true;
+			options.verticalScale = scaleTo;
+			options.horizontalScale = scaleTo;
+
+			document.exportFile(file, ExportType.PNG24, options);
+		};
+	}
+}
---
+++ iOS.jsx
@@ -0,0 +1,43 @@
+﻿/**
+* Remixer: @herkulano (http://www.herkulano.com)
+* Thanks to: Niels Bosma (niels.bosma@motorola.com)
+*/
+var document = app.activeDocument;
+var folder = new Folder("C:/Users/Jonas.Alvarado/Desktop/" + document.name.slice (0, -3));
+if (!folder.exists) {
+		folder.create();
+	}
+
+
+if (document && folder) {
+	saveToRes(50, 50, "");
+	saveToRes(100, 100, "@2x");
+     saveToRes(150, 150, "@3x");
+}
+
+function saveToRes(scaleToX, scaleToY, densitySuffix) {
+	var i, ab,
+		file, options;
+
+	for (i = document.artboards.length - 1; i >= 0; i--) {
+		document.artboards.setActiveArtboardIndex(i);
+		ab = document.artboards[i];
+        
+     var fatherFolder = new Folder(folder.fsName + "/" + ab.name + ".imageset");
+    
+    if (!fatherFolder.exists) {
+		fatherFolder.create();
+	}
+        
+		file = new File(fatherFolder.fsName + "/" + ab.name + densitySuffix + ".png");
+
+		options = new ExportOptionsPNG24();
+		options.antiAliasing = true;
+		options.transparency = true;
+		options.artBoardClipping = true;
+		options.verticalScale = scaleToY;
+		options.horizontalScale = scaleToX;
+
+		document.exportFile(file, ExportType.PNG24, options);
+	}
+}
